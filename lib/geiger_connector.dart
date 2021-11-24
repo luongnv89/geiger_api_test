56026d199f236ffb616c6fd41d00f3a1ef8b7cdd
Luong NGUYEN <luongnv89@gmail.com>
2021-11-24 11:54:40 +0100

    first attemp with geiger_api

--- lib/geiger_connector.dart
+++ lib/geiger_connector.dart
@@ -1,41 +1,43 @@
 import 'dart:developer';
 
+import 'package:geiger_api/geiger_api.dart';
 import 'package:geiger_localstorage/geiger_localstorage.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 
 class GeigerConnector {
-  late StorageController geigerToolboxStorageController;
+  late GeigerApi? geigerApi;
+  late StorageController? geigerToolboxStorageController;
   Future<void> initGeigerStorage() async {
-    String dbPath = join(await getDatabasesPath(), 'geiger_database.db');
-    log('Database path: $dbPath');
-    geigerToolboxStorageController =
-        GenericController('MI-Cyberrange', SqliteMapper(dbPath));
-  }
-
-  void writeToGeigerStorage(String data) {
-    log('Trying to get the data node');
     try {
-      log('Found the data node - Going to write the data');
-      Node node = geigerToolboxStorageController.get(':data-node');
-      node.addOrUpdateValue(NodeValueImpl('data', '$data'));
-      geigerToolboxStorageController.update(node);
-    } catch (e) {
+      geigerApi = await getGeigerApi('<unspecified>', 'miCyberrangePlugin');
+      geigerToolboxStorageController = geigerApi!.getStorage();
+    } on Exception catch (e) {
+      log('Cannot initialize GeigerAPI');
       log(e.toString());
-      log('Cannot find the data node - Going to create a new one');
-      Node node = NodeImpl('data-node');
-      geigerToolboxStorageController.addOrUpdate(node);
-      node.addOrUpdateValue(NodeValueImpl('data', '$data'));
-      geigerToolboxStorageController.update(node);
     }
   }
 
-  String? readDataFromGeigerStorage() {
+  Future<void> writeToGeigerStorage(String data) async {
+    try {
+      log('Found the data node - Going to write the data');
+      Node node = await geigerToolboxStorageController!.get(':data-node');
+      await node.addOrUpdateValue(NodeValueImpl('data', '$data'));
+      await geigerToolboxStorageController!.update(node);
+    } catch (e) {
+      log(e.toString());
+      log('Cannot find the data node - Going to create a new one');
+      Node node = NodeImpl('data-node', '');
+      await geigerToolboxStorageController!.addOrUpdate(node);
+      await node.addOrUpdateValue(NodeValueImpl('data', '$data'));
+      await geigerToolboxStorageController!.update(node);
+    }
+  }
+
+  Future<String?> readDataFromGeigerStorage() async {
     log('Trying to get the data node');
     try {
       log('Found the data node - Going to get the data');
-      Node node = geigerToolboxStorageController.get(':data-node');
-      NodeValue? nValue = node.getValue('data');
+      Node node = await geigerToolboxStorageController!.get(':data-node');
+      NodeValue? nValue = await node.getValue('data');
       if (nValue != null) {
         return nValue.value;
       } else {
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,45 @@
 import 'dart:async';
 import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:toolbox_api_test/geiger_connector.dart';
-import 'package:toolbox_api_test/utils.dart';
+import 'package:geiger_api/geiger_api.dart';
+import 'package:permission_handler/permission_handler.dart';
+// import 'package:toolbox_api_test/geiger_connector.dart';
+// import 'package:toolbox_api_test/utils.dart';
 
-GeigerConnector geigerConnector = GeigerConnector();
+// GeigerConnector geigerConnector = GeigerConnector();
+bool isReady = false;
+GeigerApi? geigerApi;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await readPackageInfo();
-  await geigerConnector.initGeigerStorage();
+  // await readPackageInfo();
+  isReady = await checkPermissions();
+  if (isReady) {
+    await initGeigerAPI();
+  }
   runApp(MyApp());
+}
+
+Future<void> initGeigerAPI() async {
+  try {
+    geigerApi = await getGeigerApi('<unspecified>', 'miCyberrangePlugin');
+  } catch (e) {
+    log('Failed to get the GeigerAPI');
+    log(e.toString());
+  }
+}
+
+Future<bool> checkPermissions() async {
+  final storagePermissionStatus = await Permission.storage.status;
+  print('storagePermissionStatus: $storagePermissionStatus');
+  final manageExternalStoragePermissionStatus =
+      await Permission.manageExternalStorage.status;
+  print(
+      'manageExternalStoragePermissionStatus: $manageExternalStoragePermissionStatus');
+  if (storagePermissionStatus == PermissionStatus.granted &&
+      manageExternalStoragePermissionStatus == PermissionStatus.granted) {
+    return true;
+  }
+  return false;
 }
 
 class MyApp extends StatelessWidget {
@@ -29,42 +59,72 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   // Get battery level.
-  String geigerData = geigerConnector.readDataFromGeigerStorage() ?? 'Failed';
-  TextEditingController inputDataController = TextEditingController();
+  // String geigerData = 'Failed';
+  // TextEditingController inputDataController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Geiger APIs - Version ${getVersion()}"),
+        title: Text("Geiger APIs - ${DateTime.now().toIso8601String()}"),
       ),
       body: Container(
         margin: EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            TextField(
-              controller: inputDataController,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                log('Enter data: ${inputDataController.text}');
-                String inputData = inputDataController.text.trim();
-                if (inputData != '') {
-                  geigerConnector.writeToGeigerStorage(inputData);
-                  inputDataController.clear();
-                  setState(() {
-                    geigerData = geigerConnector.readDataFromGeigerStorage() ??
-                        'Failed!';
-                  });
-                }
-              },
-              child: const Text('Save to Geiger Storage'),
-            ),
-            SizedBox(height: 20),
-            Text('Geiger Data: $geigerData'),
+            // TextField(
+            //   controller: inputDataController,
+            // ),
+            // ElevatedButton(
+            //   onPressed: () async {
+            //     log('Enter data: ${inputDataController.text}');
+            //     String inputData = inputDataController.text.trim();
+            //     if (inputData != '') {
+            //       await geigerConnector.writeToGeigerStorage(inputData);
+            //       inputDataController.clear();
+            //       String? newData =
+            //           await geigerConnector.readDataFromGeigerStorage();
+            //       setState(() {
+            //         geigerData = newData ?? 'Failed!';
+            //       });
+            //     }
+            //   },
+            //   child: const Text('Save to Geiger Storage'),
+            // ),
+            // SizedBox(height: 20),
+            // Text('Geiger Data: $geigerData'),
+            isReady == false
+                ? ElevatedButton(
+                    onPressed: () async {
+                      await _requestPermissions(Permission.storage);
+                      await _requestPermissions(
+                          Permission.manageExternalStorage);
+                      initGeigerAPI();
+                    },
+                    child: Text('Request Permissions'),
+                  )
+                : Text('All permissions been granted!'),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> _requestPermissions(Permission permission) async {
+    var status = await permission.status;
+    if (status == PermissionStatus.granted) {
+      print('OK! Permission granted');
+    } else if (status == PermissionStatus.denied) {
+      print('Permission denied. Going to ask for permission');
+      status = await permission.request();
+      if (status == PermissionStatus.granted) {
+        print('Awesome! Permission granted');
+      } else if (status == PermissionStatus.denied) {
+        print('Ooop! Permission denied');
+      }
+    } else if (status == PermissionStatus.permanentlyDenied) {
+      print('Take the user to the settings page.');
+      await openAppSettings();
+    }
   }
 }
